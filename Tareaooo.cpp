7e5c0def9930bcd7a2f49f0094f9bf7351654899
Alejandro <ale_arc2015@hotmail.com>
2019-04-04 20:01:36 -0300

    Tarea 3

--- Tareaooo.cpp
+++ Tareaooo.cpp
@@ -2,18 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <conio.h>
-
+//Declaracion de estructuras
+enum TRaza{Orco, Humano, Mago, Enano, Elfo};
+char Nombres[5][20]={"Merodeador", "Rompehuesos", "Algoritmico", "Proscripto", "Cruzamontanias"};
+char Apellidos[5][20]={" Furioso", " Mistico", " Peregrino", " Galactico", " Sabio"};
+//Estructura de Datos
+struct TDatos {
+	TRaza Raza; //nota 1
+	char *ApellidoNombre; //nota 2
+	int edad; //entre 0 a 300
+	double Salud;//100
+} typedef DATOS;
+//Estructura de Caracteristicas
+struct TCaracteristicas{
+	int velocidad;// 1 a 10
+	int destreza; //1 a 5
+	int fuerza;//1 a 10
+	int Nivel; //1 a 10
+	int Armadura; //1 a 10
+} typedef CARACTERISTICAS;
+//Estructura de Personaje
+struct TPersonaje {
+	TDatos * DatosPersonales;  // null
+	TCaracteristicas * Caracteristicas; // null 
+} typedef PERSONAJE;
+//Declaracion de funciones
 void cargarcaract(struct TCaracteristicas * CaracPers);
 void mostrarcaract(struct TCaracteristicas * CaracPers);
 void cargar_datos(struct TDatos *puntero);
 void mostrar_datos(struct TDatos *a);
-
-
+//Main
 int main(void){
-
+	srand(time(NULL));//Randomiza la seed
+	//Declaro un puntero de tipo PERSONAJE
+	PERSONAJE *personajes = NULL;
+	int cant;
+	printf("Cuantos personajes desea crear?");
+	scanf("%d",&cant);
+	//Asigno la memoria correspondiente a la lista de personajes
+	personajes = (PERSONAJE*) malloc(sizeof(PERSONAJE)*cant);
+	//Cargo los personajes
+	for (int i = 0; i < cant; ++i)
+	{
+		//Asigno la memoria a las dos estructuras pertenecientes a personajes
+		personajes[i].DatosPersonales = (DATOS*) malloc(sizeof(DATOS));
+		personajes[i].Caracteristicas = (CARACTERISTICAS*) malloc(sizeof(CARACTERISTICAS));
+		cargar_datos(personajes[i].DatosPersonales);
+		cargarcaract(personajes[i].Caracteristicas);
+	}
+	//Muestro los datos
+	for (int j = 0; j < cant; ++j)
+	{
+		mostrar_datos(personajes[j].DatosPersonales);
+		mostrarcaract(personajes[j].Caracteristicas);
+	}
 	return 0;
 }
+//Definicion de funciones
+//Funciones de Caracteristicas
 void cargarcaract(struct TCaracteristicas * CaracPers){
 	CaracPers->velocidad = rand()%10 + 1;
 	CaracPers->destreza = rand()%5 + 1;
@@ -21,7 +67,6 @@ void cargarcaract(struct TCaracteristicas * CaracPers){
 	CaracPers->Nivel = rand()%10 + 1;
 	CaracPers->Armadura = rand()%10 + 1;
 }
-
 void mostrarcaract(struct TCaracteristicas * CaracPers){
 	printf("Velocidad: %d\n", CaracPers->velocidad);
 	printf("Destreza: %d\n", CaracPers->destreza);
@@ -29,7 +74,7 @@ void mostrarcaract(struct TCaracteristicas * CaracPers){
 	printf("Nivel: %d\n", CaracPers->Nivel);
 	printf("Armadura: %d\n", CaracPers->Armadura);
 }
-
+//Funciones de Datos
 void cargar_datos(struct TDatos *puntero){
 	char AM[100];
 	strcpy(AM, Nombres[rand()%5]);
@@ -56,11 +101,8 @@ void cargar_datos(struct TDatos *puntero){
 	puntero->edad = rand()%301;
 	puntero->Salud = 100;
 	printf("\n");
-
 }
-
 void mostrar_datos(struct TDatos *a){
-	
 	printf("\nRaza:");
 	switch(a->Raza){
 		case 0:
@@ -84,3 +126,4 @@ void mostrar_datos(struct TDatos *a){
 	printf("\nSalud: %.2lf", a->Salud);
 	printf("\n");
 }
+//Daño provocado (VA-PDEF)/MDP)*100
