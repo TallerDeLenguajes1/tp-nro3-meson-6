5c6082b8e0b8a7310cb5514f1596b3141aa59ec9
Juan Castelli <juancastelli12345@gmail.com>
2019-04-04 19:55:43 -0300

    Gameplay Grupo 2

---
+++ Gameplay.cpp
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <conio.h>
+#define MDP 50000.000
+
+enum TRaza{Orco, Humano, Mago, Enano, Elfo};
+char Nombres[5][20]={"Merodeador", "Rompehuesos", "Algoritmico", "Proscripto", "Cruzamontanias"};
+char Apellidos[5][20]={" Furioso", " Mistico", " Peregrino", " Galactico", " Sabio"};
+
+struct TDatos {
+	TRaza Raza; //nota 1
+	char *ApellidoNombre; //nota 2
+	int edad; //entre 0 a 300
+	double Salud;//100
+};
+struct TCaracteristicas{
+	int velocidad;// 1 a 10
+	int destreza; //1 a 5
+	int fuerza;//1 a 10
+	int Nivel; //1 a 10
+	int Armadura; //1 a 10
+};
+struct TPersonaje {
+	TDatos * DatosPersonales;  // null
+	TCaracteristicas * Caracteristicas; // null 
+};
+void cargarcaract(struct TCaracteristicas * CaracPers);
+void mostrarcaract(struct TCaracteristicas * CaracPers);
+void cargar_datos(struct TDatos *puntero);
+void mostrar_datos(struct TDatos *a);
+
+int main(void){
+	srand(time(NULL));
+	struct TPersonaje * Lista;
+	int cant;
+	printf("Cuantos personajes desea cargar?:\n");
+	scanf("%d", &cant);
+	Lista = (struct TPersonaje *) malloc(sizeof(struct TPersonaje)*cant);
+	for (int t = 0; t < cant; ++t)
+	{
+		fflush(stdin);
+		Lista[t].DatosPersonales = (struct TDatos * )malloc(sizeof(struct TDatos));
+		Lista[t].Caracteristicas = (struct TCaracteristicas * )malloc(sizeof(struct TCaracteristicas));
+		cargar_datos(Lista[t].DatosPersonales); // (personaje . datospersonales) (* Tdatos)
+		cargarcaract(Lista[t].Caracteristicas);
+	}
+	struct TPersonaje personaje1 = Lista[rand()%cant];
+	struct TPersonaje personaje2 = Lista[rand()%cant];
+	mostrar_datos(personaje1.DatosPersonales);
+	mostrarcaract(personaje1.Caracteristicas);
+	printf("\nVS\n");
+	mostrar_datos(personaje2.DatosPersonales);
+	mostrarcaract(personaje2.Caracteristicas);
+
+
+	printf("-------------------------FIGHT!!!!-------------------------\n");
+	int PD1, PD2, ED1, ED2, VA1, VA2, PDEF1, PDEF2;
+	double Danio1,Danio2;
+	PD1 = personaje1.Caracteristicas->destreza * personaje1.Caracteristicas->fuerza * personaje1.Caracteristicas->Nivel;
+	PD2 = personaje2.Caracteristicas->destreza * personaje2.Caracteristicas->fuerza * personaje2.Caracteristicas->Nivel;
+	for (int i = 0; i < 3; ++i)
+	{
+		if(personaje1.DatosPersonales->Salud <= 0 || personaje2.DatosPersonales->Salud <= 0){
+			break;
+		}
+		ED1 = rand() % 100 + 1;
+		ED2 = rand() % 100 + 1;
+		VA1 = PD1 * ED1;
+		VA2 = PD2 * ED2;
+		PDEF1 = personaje1.Caracteristicas->Armadura * personaje1.Caracteristicas->velocidad;
+		PDEF2 = personaje2.Caracteristicas->Armadura * personaje2.Caracteristicas->velocidad;
+		Danio1 = ((VA1 - PDEF2)/MDP)*100; //daño que se provoca al segundo jugador
+		Danio2 = ((VA2 - PDEF1)/MDP)*100; //daño que se provoca al primer jugador
+		personaje1.DatosPersonales->Salud = personaje1.DatosPersonales->Salud - Danio2;
+		personaje2.DatosPersonales->Salud = personaje2.DatosPersonales->Salud - Danio1;		
+	}
+
+	/*for (int k = 0; k < cant; ++k)
+	{
+		printf("\nPersonaje %d:\n", (k+1));
+		mostrar_datos(Lista[k].DatosPersonales);
+		mostrarcaract(Lista[k].Caracteristicas);
+	}*/
+	if (personaje1.DatosPersonales->Salud > personaje2.DatosPersonales->Salud)
+	{
+		printf("Gana Personaje 1!!!!\n");
+	}else if(personaje1.DatosPersonales->Salud < personaje2.DatosPersonales->Salud){ 
+		printf("Gana Personaje 2!!!!\n");
+	}else{
+		printf("Menudo empate\n");
+	}
+	return 0;
+}
+
+void cargarcaract(struct TCaracteristicas * CaracPers){
+	CaracPers->velocidad = rand()%10 + 1;
+	CaracPers->destreza = rand()%5 + 1;
+	CaracPers->fuerza = rand()%10 + 1;
+	CaracPers->Nivel = rand()%10 + 1;
+	CaracPers->Armadura = rand()%10 + 1;
+}
+
+void mostrarcaract(struct TCaracteristicas * CaracPers){
+	printf("Velocidad: %d\n", CaracPers->velocidad);
+	printf("Destreza: %d\n", CaracPers->destreza);
+	printf("Fuerza: %d\n", CaracPers->fuerza);
+	printf("Nivel: %d\n", CaracPers->Nivel);
+	printf("Armadura: %d\n", CaracPers->Armadura);
+}
+
+void cargar_datos(struct TDatos *puntero){
+	char AM[100];
+
+	strcpy(AM, Nombres[rand()%5]);
+	strcat(AM, Apellidos[rand()%5]);
+	puntero->ApellidoNombre = (char*) malloc(sizeof(char) * strlen(AM) + 2);
+
+	switch(rand()%5){
+		case 0:
+			puntero->Raza = Orco;
+			break;
+		case 1:
+			puntero->Raza = Humano;
+			break;
+		case 2:
+			puntero->Raza = Mago;
+			break;
+		case 3:
+			puntero->Raza = Enano;
+			break;
+		case 4:
+			puntero->Raza = Elfo;
+			break;
+	}
+	strcpy(puntero->ApellidoNombre, AM);
+	puntero->edad = rand()%301;
+	puntero->Salud = 100;
+	printf("\n");
+
+}
+
+void mostrar_datos(struct TDatos *a){
+	
+	printf("\nRaza:");
+	switch(a->Raza){
+		case 0:
+			printf("Orco\n");
+			break;
+		case 1:
+			printf("Humano\n");
+			break;
+		case 2:
+			printf("Mago\n");
+			break;
+		case 3:
+			printf("Enano\n");
+			break;
+		case 4:
+			printf("Elfo\n");
+			break;
+	}
+	printf("Apellido y Nombre: %s \n", a->ApellidoNombre);
+	printf("Edad: %d", a->edad);
+	printf("\nSalud: %.2lf", a->Salud);
+	printf("\n");
+
+}
---
+++ README.md.txt
@@ -0,0 +1,21 @@
+Elija 2 personajes para que compitan entre ellos. Cada uno tendrá 3 ataques que irán
+debilitando al oponente. Al final de dichas rondas el que mejor salud tenga será
+declarado ganador. ( puede haber empate)
+La forma de valorar la efectividad del ataque es la siguiente:
+Valores de Ataque
+? Poder de Disparo: Haga el producto de Destreza * Fuerza * Nivel del
+personaje que ataca (PD)
+? Efectividad de Disparo:Genere un valor aleatorio de 1 a 100. Considerarlo
+como valor porcentual (ED)
+? Valor de Ataque:Al Poder de Disparo lo multiplico por la Efectividad de
+Disparo. (VA)
+Valores de Defensa
+? Poder de Defensa: Haga el producto de Armadura * Velocidad del personaje
+que defiende (PDEF)
+Resultado del enfrentamiento
+? máximo daño provocable: 50000 (MDP)
+? Daño provocado: Valor de Ataque*Efectividad de Disparo - Poder de Defensa
+todo eso dividido por el máximo daño provocado (50000) y a todo eso lo
+multiplico por 100 ? ((VA*EP)-PDEF)/MDP)*100
+? Actualizar salud del personaje que se defiende Restándole a Salud el Daño
+provocado.
