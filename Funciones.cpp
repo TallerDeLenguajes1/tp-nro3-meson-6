0af8c6e670f9d200cc77c5ac958aa849be32112d
Alejandro <ale_arc2015@hotmail.com>
2019-04-04 19:25:35 -0300

    funciones

---
+++ Funciones.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <conio.h>
+
+
+
+void cargarcaract(struct TCaracteristicas * CaracPers);
+void mostrarcaract(struct TCaracteristicas * CaracPers);
+void cargar_datos(struct TDatos *puntero);
+void mostrar_datos(struct TDatos *a);
+
+
+int main(void){
+
+	return 0;
+}
+void cargarcaract(struct TCaracteristicas * CaracPers){
+	CaracPers->velocidad = rand()%10 + 1;
+	CaracPers->destreza = rand()%5 + 1;
+	CaracPers->fuerza = rand()%10 + 1;
+	CaracPers->Nivel = rand()%10 + 1;
+	CaracPers->Armadura = rand()%10 + 1;
+}
+
+void mostrarcaract(struct TCaracteristicas * CaracPers){
+	printf("Velocidad: %d\n", CaracPers->velocidad);
+	printf("Destreza: %d\n", CaracPers->destreza);
+	printf("Fuerza: %d\n", CaracPers->fuerza);
+	printf("Nivel: %d\n", CaracPers->Nivel);
+	printf("Armadura: %d\n", CaracPers->Armadura);
+}
+
+void cargar_datos(struct TDatos *puntero){
+	char AM[100];
+
+	strcpy(AM, Nombres[rand()%5]);
+	strcat(AM, Apellidos[rand()%5]);
+	puntero->ApellidoNombre = (char*) malloc(sizeof(char) * strlen(AM) + 2);
+	switch(rand()%5){
+		case 0:
+			puntero->Raza = Orco;
+			break;
+		case 1:
+			puntero->Raza = Humano;
+			break;
+		case 2:
+			puntero->Raza = Mago;
+			break;
+		case 3:
+			puntero->Raza = Enano;
+			break;
+		case 4:
+			puntero->Raza = Elfo;
+			break;
+	}
+	strcpy(puntero->ApellidoNombre, AM);
+	puntero->edad = rand()%301;
+	puntero->Salud = 100;
+	printf("\n");
+
+}
+
+void mostrar_datos(struct TDatos *a){
+	
+	printf("\nRaza:");
+	switch(a->Raza){
+		case 0:
+			printf("Orco\n");
+			break;
+		case 1:
+			printf("Humano\n");
+			break;
+		case 2:
+			printf("Mago\n");
+			break;
+		case 3:
+			printf("Enano\n");
+			break;
+		case 4:
+			printf("Elfo\n");
+			break;
+	}
+	printf("Apellido y Nombre: %s \n", a->ApellidoNombre);
+	printf("Edad: %d", a->edad);
+	printf("\nSalud: %.2lf", a->Salud);
+	printf("\n");
+}
