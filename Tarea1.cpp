fbe1b4859baa5877ec361268d6b6a6dbec11622a
SantiagoToledo <santi.nt2@gmail.com>
2019-05-03 23:35:30 -0300

    termine funciones de lista

--- Tarea1.cpp
+++ Tarea1.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <time.h>
 #include <conio.h>
+#define indef -999
 
 enum TRaza{Orco, Humano, Mago, Enano, Elfo};
 char Nombres[5][20]={"Merodeador", "Rompehuesos", "Algoritmico", "Proscripto", "Cruzamontanias"};
@@ -27,11 +28,11 @@ struct TCaracteristicas{
 struct TPersonaje {
 	struct TDatos * DatosPersonales;  // null
 	struct TCaracteristicas * Caracteristicas; // null 
+	int id;
 };
 
 struct nodo{
 	struct TPersonaje pj;
-	int id;
 	struct nodo *siguiente;
 };
 
@@ -42,17 +43,13 @@ lista insertarIni(lista);
 lista insertarFin(lista);
 lista eliminar(lista);
 lista eliminarX(lista,int);
-TPersonaje buscar(lista);
-
-
+lista buscar(lista,int);
 
 void cargarCaract(struct TCaracteristicas * CaracPers);
 void mostrarCaract(struct TCaracteristicas * CaracPers);
 void cargarDatos(struct TDatos *puntero);
 void mostrarDatos(struct TDatos *a);
-void juego(struct TPersonaje*);
-
-
+void juego(lista);
 
 
 int main(void){
@@ -61,40 +58,18 @@ int main(void){
 	
 	lista jugadores = NULL;
 	int p1, p2;
-	
 	int cant;
 	
 	printf("Cuantos personajes desea cargar?:\n");
 	scanf("%d", &cant);
-	
 	fflush(stdin);
-
 	
 	for (int t = 0; t < cant; t++)
 	{	
-
-		struct TPersonaje *nuevo;
-		nuevo = (struct TPersonaje*) malloc(sizeof(struct TPersonaje));
-
-		nuevo->siguiente = Lista;
-		Lista = nuevo;
-	
-		Lista->DatosPersonales = (struct TDatos * )malloc(sizeof(struct TDatos));
-		Lista->Caracteristicas = (struct TCaracteristicas * )malloc(sizeof(struct TCaracteristicas));
-		
-		cargarDatos(Lista->DatosPersonales); 
-		cargarCaract(Lista->Caracteristicas);
+		jugadores = insertarIni(jugadores);
 	}
 
-	for (nuevo = Lista; nuevo != NULL ; nuevo = nuevo->siguiente)
-	{	
-		mostrarDatos(nuevo->DatosPersonales);
-		mostrarCaract(nuevo->Caracteristicas);
-	}
-
-
-	juego(Lista);
-
+	juego(jugadores);
 
 	return 0;
 }
@@ -174,72 +149,61 @@ void mostrarDatos(struct TDatos *a){
 }
 
 //------------JUEGO-------------------------
-void juego(struct TPersonaje *Lista){
+void juego(lista l){
 	
 	int per1, per2, cont=0, i=1;
 	float PD1, ED1, VA1, PDEF1, DAN1, PD2, ED2, VA2, PDEF2, DAN2, MDP=50000;
 	
-	struct TPersonaje *nuevo = Lista, p1, p2;
+	struct nodo *p1 = NULL, *p2 = NULL;
 
 	printf("\n Ingrese primer personaje que va a pelear: ");
 	scanf("%d",&per1);
 	printf("\n Ingrese segundo personaje que va a pelear: ");
 	scanf("%d",&per2);
-
-	while( nuevo != NULL ){
-		
-		cont = cont +1;
-		
-		if(cont == per1){
-			p1 = *nuevo;
-		}
-		
-		if(cont == per2){
-			p2 = *nuevo;
-		}
-		nuevo = nuevo->siguiente;
-	}
-	printf("salio while");
 	
+	p1= buscar(l,per1);
+	p2= buscar(l,per2);
 
-	mostrarDatos(p1.DatosPersonales);
-	mostrarCaract(p1.Caracteristicas);
+	mostrarDatos(p1->pj.DatosPersonales);
+	mostrarCaract(p1->pj.Caracteristicas);
 
-	mostrarDatos(p2.DatosPersonales);
-	mostrarCaract(p2.Caracteristicas);
+	mostrarDatos(p2->pj.DatosPersonales);
+	mostrarCaract(p2->pj.Caracteristicas);
+
+
 			
-	while(i <= 3 &&  p1.DatosPersonales->Salud>0 && p2.DatosPersonales->Salud >0){
+	while(i <= 3 &&  p1->pj.DatosPersonales->Salud>0 && p2->pj.DatosPersonales->Salud >0){
 		
 		printf("\n---------ROUND %d---------\n", i);
 		
-		PD1 = p1.Caracteristicas->destreza * p1.Caracteristicas->fuerza * p1.Caracteristicas->Nivel;
+		PD1 = p1->pj.Caracteristicas->destreza * p1->pj.Caracteristicas->fuerza * p1->pj.Caracteristicas->Nivel;
 		ED1 = rand()%99 +1;
 		VA1 = (PD1 * ED1);
-		PDEF1 = p1.Caracteristicas->Armadura * p1.Caracteristicas->velocidad;
+		PDEF1 = p1->pj.Caracteristicas->Armadura * p1->pj.Caracteristicas->velocidad;
 
-		PD2 = p2.Caracteristicas->destreza * p2.Caracteristicas->fuerza * p2.Caracteristicas->Nivel;
+		PD2 = p2->pj.Caracteristicas->destreza * p2->pj.Caracteristicas->fuerza * p2->pj.Caracteristicas->Nivel;
 		ED2 = rand()%99 +1;
 		VA2 = PD2 * ED2;
-		PDEF2 = p2.Caracteristicas->Armadura * p2.Caracteristicas->velocidad;
+		PDEF2 = p2->pj.Caracteristicas->Armadura * p2->pj.Caracteristicas->velocidad;
 
 		DAN1 = ((VA1-PDEF2)/MDP)*100;
 		DAN2 = ((VA2-PDEF1)/MDP)*100;
 
-		p1.DatosPersonales->Salud = p1.DatosPersonales->Salud - DAN2;
-		p2.DatosPersonales->Salud = p2.DatosPersonales->Salud - DAN1;
+		p1->pj.DatosPersonales->Salud = p1->pj.DatosPersonales->Salud - DAN2;
+		p2->pj.DatosPersonales->Salud = p2->pj.DatosPersonales->Salud - DAN1;
 
-		printf("\n%s\n  Danio recibido: %.2f  | Salud: %.2f ", p1.DatosPersonales->ApellidoNombre, DAN2, p1.DatosPersonales->Salud);
-		printf("\n%s\n  Danio recibido: %.2f  | Salud: %.2f ", p2.DatosPersonales->ApellidoNombre, DAN1, p2.DatosPersonales->Salud);
+		printf("\n%s\n  Danio recibido: %.2f  | Salud: %.2f ", p1->pj.DatosPersonales->ApellidoNombre, DAN2, p1->pj.DatosPersonales->Salud);
+		printf("\n%s\n  Danio recibido: %.2f  | Salud: %.2f ", p2->pj.DatosPersonales->ApellidoNombre, DAN1, p2->pj.DatosPersonales->Salud);
 		i = i+1;
 
 	}
-	if(p1.DatosPersonales->Salud == p2.DatosPersonales->Salud){
+	if(p1->pj.DatosPersonales->Salud == p2->pj.DatosPersonales->Salud){
 		printf("\n ========EMPATE=========");
 	}else{
-		if(p1.DatosPersonales->Salud > p2.DatosPersonales->Salud){
-			printf("\n\n=========== Ganador: %s ===============", p1.DatosPersonales->ApellidoNombre);
+		if(p1->pj.DatosPersonales->Salud > p2->pj.DatosPersonales->Salud){
+			printf("\n\n=========== Ganador: %s ===============", p1->pj.DatosPersonales->ApellidoNombre);
 		}else{
-			printf("\n\n=========== Ganador: %s ===============", p2.DatosPersonales->ApellidoNombre);
+			printf("\n\n=========== Ganador: %s ===============", p2->pj.DatosPersonales->ApellidoNombre);
 		}
 	}
 }
@@ -282,11 +246,11 @@ lista insertarIni(lista l){
 
 	nuevo->pj.DatosPersonales = (struct TDatos * )malloc(sizeof(struct TDatos));
 	nuevo->pj.Caracteristicas = (struct TCaracteristicas * )malloc(sizeof(struct TCaracteristicas));
-
+	
+	nuevo->pj.id = rand()%999+1;
 	cargarDatos(nuevo->pj.DatosPersonales); 
 	cargarCaract(nuevo->pj.Caracteristicas);
 
-	nuevo->id = rand()%999+1;
 
 	nuevo->siguiente = l;
 	l = nuevo;
@@ -304,26 +268,52 @@ lista insertarFin(lista l){
 	nuevo->pj.DatosPersonales = (struct TDatos * )malloc(sizeof(struct TDatos));
 	nuevo->pj.Caracteristicas = (struct TCaracteristicas * )malloc(sizeof(struct TCaracteristicas));
 
+	nuevo->pj.id = rand()%999+1;
 	cargarDatos(nuevo->pj.DatosPersonales); 
 	cargarCaract(nuevo->pj.Caracteristicas);
 
-	while(aux != NULL ){
+	while(aux->siguiente != NULL ){
 		aux = aux->siguiente;
 	}
+	nuevo->siguiente = aux->siguiente;
+	aux->siguiente = nuevo;
 
-
-
+	return l; 
 }
 	
 
+lista eliminar(lista l){
+	struct nodo *aux;
+	aux = l;
+	l = l->siguiente;
+	free(aux); 
+
+	return l; 
+}
 
 
+lista eliminarX(lista l,int x){
+	lista aux = l;
+	struct nodo *auxborrar;
+	while( aux != NULL && aux->siguiente->pj.id != x && aux->pj.id !=x){
+		aux = aux->siguiente;
+	}
+
+	if(aux == NULL){
+		printf("\nNo se encontro %d para eliminar.", x);
+	}else{
+		auxborrar= aux->siguiente;
+		aux->siguiente = aux->siguiente->siguiente;
+		free(auxborrar);
+	}
+	return l; 
+}
 
 
-
-
-
-
-lista eliminar(lista);
-lista eliminarX(lista,int);
-TPersonaje buscar(lista);
+lista buscar(lista l, int x){
+	lista aux = l; 
+	while(aux != NULL && aux->pj.id != x ){
+		aux = aux->siguiente;
+	}
+	return aux;
+}
